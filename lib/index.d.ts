484e485175d7ab5fc0c233ae16524eff2b6dec72
上海深普软件.Co.David <leev@smpoo.com>
2021-03-30 17:05:13 +0800

    (2021-03-30 17:05:13)test d.ts again +7

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { tMsg } from './types/global';
-export declare const Tutil: (str: string) => string;
-export declare const Techo: (msg: tMsg) => void;
-export declare const Tfunc: (val: string | number) => string;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { tMsg } from './types/global';
+import { tmind } from './types/global';
 
 const _tUtil_ = (str: string): string => str;
 
-const _tEcho_ = (msg: tMsg): void => {
+const _tEcho_ = (msg: tmind.tMsg): void => {
 	if (msg === 'info') console.log('info'); // eslint-disable-line
 	else if (msg === 'warn') console.log('warn'); // eslint-disable-line
 	else if (msg === 'err') console.log('err'); // eslint-disable-line
