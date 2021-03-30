60d50736c0b2184620b98f1cd71e5eaaad468488
上海深普软件.Co.David <leev@smpoo.com>
2021-03-30 17:21:57 +0800

    (2021-03-30 17:21:57)test d.ts +9

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,6 +6,10 @@ declare module tmind {
 	function Tutil(str: string): string;
 	function Techo(msg: tmind.tMsg): void;
 	function Tfunc(val: string | number): string;
+	/** 测试方法
+	 *  仅用于d.ts文件智能感知测试
+	 * @param str
+	 */
 	function Ttest(str: string): void;
 }
 
